78ed58a43c9ddb5164156982059b1afd1fb6e05b
David <david.r.fankhauser@gmail.com>
2016-09-07 21:25:52 -0400

    fixed map issues

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,19 +15,19 @@
 //= require_tree .
 $(document).ready(function (){
 
-    function initialize() {
-      var mapOptions = {
-      center: { lat: -34.397, lng: 150.644},
-      zoom: 8
-      };
-        
-      var map = new google.maps.Map(document.getElementById('map-canvas'),
-                mapOptions);    
-    }
-      
-    google.maps.event.addDomListener(window, 'load', initialize);
-});
+   function initialize() {
+  var myLatlng = new google.maps.LatLng(35.227087,-80.843127);
+  
+  var mapOptions = {
+    zoom: 8,
+    center: myLatlng,
+    scrollwheel: false    
+  }
 
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
 
 
 function codeAddress() {
